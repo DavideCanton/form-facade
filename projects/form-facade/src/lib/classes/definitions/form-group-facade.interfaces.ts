e89adc67bb018e071032b2ee3ae196f6c5c5fde4
Davide Canton <davide.canton5@gmail.com>
2022-07-07 17:33:45 +0200

    simplification

--- projects/form-facade/src/lib/classes/definitions/form-group-facade.interfaces.ts
+++ projects/form-facade/src/lib/classes/definitions/form-group-facade.interfaces.ts
@@ -12,14 +12,12 @@ export const CUSTOM_VALIDATOR_SYMBOL = Symbol('custom_validator');
 /**
  * Enum corresponding to {@link FormControlStatus}
  */
-export enum ValidationStatus
-{
+export enum ValidationStatus {
   VALID = 'VALID',
   INVALID = 'INVALID',
   PENDING = 'PENDING',
   DISABLED = 'DISABLED'
 }
-
 
 /**
  * Defines a disabling condition depending on another field of the facade.
@@ -27,30 +25,31 @@ export enum ValidationStatus
  * For example `{ name: 'name', operator: map(v => v == 'John') }` means that
  * the field will be disabled if the value of the field `name` is `John`.
  */
-export type IDisabledWhenField<T, TK extends keyof T = keyof T> = {
+export type IDisabledWhenField<T> = {
   [K in keyof T]: {
     /** the name of the field */
     name: K;
     /** the operator to apply to convert the current value to a boolean (`true` if disabled) */
     operator: OperatorFunction<T[K], boolean>;
   }
-}[TK];
+}[keyof T];
 
 /**
  * Defines a whole disabling condition on a field of the form depending on many fields.
  * It executes all the conditions using {@link combineLatest}, then joins the outcomes
  * using the provided {@link joiner}, or {@link _.some} if no {@link joiner} is provided.
  */
-export interface DisabledWhenMultipleFields<T>
-{
+export interface DisabledWhenMultipleFields<T> {
   /** the array of conditions, depending on multiple fields */
   conditions: IDisabledWhenField<T>[];
-  /** the combination function, defaults to {@link _.some}. The input array keeps the same order of values in {@link conditions} */
+  /** 
+   * The combination function, defaults to {@link _.some}. 
+   * The input array keeps the same order of values in {@link conditions}.
+   */
   joiner?: (b: boolean[]) => boolean;
 }
 
-export interface FormDefinition<T, TParent = any>
-{
+export interface FormDefinition<T, TParent = any> {
   initialValue: T;
   validator?: ValidatorFn;
   asyncValidator?: AsyncValidatorFn;
@@ -58,8 +57,7 @@ export interface FormDefinition<T, TParent = any>
   disabledWhen?: Observable<boolean> | IDisabledWhenField<TParent> | DisabledWhenMultipleFields<TParent>;
 }
 
-export interface FormArrayDefinition<T, TParent = any> extends FormDefinition<T, TParent>
-{
+export interface FormArrayDefinition<T, TParent = any> extends FormDefinition<T, TParent> {
   // eslint-disable-next-line no-use-before-define
   controlBuilder?: () => FormGroupDefinition<ElementOf<T>> | AbstractControl;
 }
@@ -80,8 +78,7 @@ export type FormGroupValidatorDefinition<T> = Partial<{
   [K in keyof T]: Pick<FormDefinition<T[K], T>, 'validator' | 'asyncValidator'>;
 }>;
 
-export interface FormDefinitionExtras
-{
+export interface FormDefinitionExtras {
   validator: ValidatorFn | null;
   asyncValidator: AsyncValidatorFn | null;
   autoMarkAsDependents: boolean;
